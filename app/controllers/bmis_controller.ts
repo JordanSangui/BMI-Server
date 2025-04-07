2c1e907f75b85806b8db92019f3acf7e60219e62
sw.ngahane <sw.ngahane@catamania.com>
2025-04-07 14:38:27 +0200

    update imc calcul

--- app/controllers/bmis_controller.ts
+++ app/controllers/bmis_controller.ts
@@ -2,14 +2,14 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class BmisController {
 
-    private BMI: Array<{ interval: Array<number>, fr: string, en: string, comment?: string }> = [
-        { interval: [0, 16.5], fr: 'Dénutrition', en: 'Undernutrition', comment: 'Less than 16.5' },
-        { interval: [16.5, 18.4], fr: 'Maigreur', en: 'Leanness', comment: '16.5 to 18.4' },
-        { interval: [18.5, 24.9], fr: 'Corpulence normale', en: 'Normal weight', comment: '18.5 to 24.9' },
-        { interval: [25, 29.9], fr: 'Surpoids', en: 'Overweight', comment: '25 to 29.9' },
-        { interval: [30, 34.9], fr: 'Obésité modérée', en: 'Moderate obesity', comment: '30 to 34.9' },
-        { interval: [35, 39.9], fr: 'Obésité sévère', en: 'Severe obesity', comment: '35 to 39.9' },
-        { interval: [40, 9999], fr: 'Obésité massive', en: 'Massive obesity', comment: '40 and over' },
+    private BMI: Array<{ interval: Array<number>, value_fr: string, value_en: string, comment?: string, img_men : string, img_woman : string, advice_fr: string, advice_en: string }> = [
+        { interval: [0, 16.5], value_fr: 'Dénutrition', value_en: 'Undernutrition', comment: 'Less than 16.5', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : 'Augmentez vos apports en calories et en protéines avec des aliments riches et faciles à digérer. Consultez un professionnel de santé pour adapter votre alimentation.', advice_en : 'Increase your intake of calories and protein with rich and easy-to-digest foods. Consult a healthcare professional to adjust your diet.' },
+        { interval: [16.5, 18.4], value_fr: 'Maigreur', value_en: 'Leanness', comment: '16.5 to 18.4', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : 'Adoptez une alimentation plus dense en énergie et pratiquez des exercices de renforcement musculaire pour prendre du poids sainement.', advice_en : '"Follow a more energy-dense diet and engage in strength exercises to gain weight in a healthy way.'  },
+        { interval: [18.5, 24.9], value_fr: 'Corpulence normale', value_en: 'Normal weight', comment: '18.5 to 24.9', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : 'Continuez à manger équilibré et à pratiquer une activité physique régulière pour maintenir votre bonne santé.', advice_en : 'Keep eating a balanced diet and exercising regularly to maintain good health.'  },
+        { interval: [25, 29.9], value_fr: 'Surpoids', value_en: 'Overweight', comment: '25 to 29.9', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : "Réduisez les aliments transformés, privilégiez les légumes et faites au moins 30 minutes d'activité physique par jour.", advice_en : 'Reduce processed foods, prioritize vegetables, and aim for at least 30 minutes of physical activity per day.'  },
+        { interval: [30, 34.9], value_fr: 'Obésité modérée', value_en: 'Moderate obesity', comment: '30 to 34.9', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : "Demandez un suivi médical pour mettre en place un plan nutritionnel et d'activité physique adapté à votre profil.", advice_en : 'Seek medical support to establish a personalized nutrition and physical activity plan'  },
+        { interval: [35, 39.9], value_fr: 'Obésité sévère', value_en: 'Severe obesity', comment: '35 to 39.9', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : 'Un accompagnement médical multidisciplinaire est nécessaire : diététique, activité physique adaptée, et soutien psychologique.', advice_en : 'A multidisciplinary medical approach is essential: dietary counseling, adapted physical activity, and psychological support.'  },
+        { interval: [40, 9999], value_fr: 'Obésité massive', value_en: 'Massive obesity', comment: '40 and over', img_men : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', img_woman : 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg', advice_fr : 'Une intervention médicale ou chirurgicale peut être envisagée. Une prise en charge globale est indispensable.', advice_en : 'Medical or surgical intervention may be considered. A comprehensive treatment plan is essential.'  },
     ]
 
     private isBetween(x: number, interval: Array<number>): boolean {
@@ -30,23 +30,16 @@ export default class BmisController {
         const weight = params.weight // kg
         const height = params.height // m
         const gender = params.gender // 1 for male and 0 for female 
-
-        console.log(weight, height, gender)
-
+        // console.log(weight, height, gender)
         if (weight && height && gender) {
             const bmi = weight / (height * height) // bmi
-            const bmi_result: { interval: Array<number>, fr: string, en: string, comment?: string } | null | undefined = this.BMI.find((element) => this.isBetween(bmi, element.interval)) // bmi calcul
-            let advice = 'Veuillez faire du sport'
-            let image = 'https://media.ooreka.fr/public/image/obese-gros-morbide-1-main-10571460.jpg'
-
+            const bmi_result: { interval: Array<number>, value_fr: string, value_en: string, comment?: string } | null | undefined = this.BMI.find((element) => this.isBetween(bmi, element.interval)) // bmi calcul
             const result = {
                 weight,
                 height,
                 gender,
                 bmi,
                 bmi_result,
-                advice,
-                image
             }
             return response.status(200).send(result)
         }
